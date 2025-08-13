bd08f9f72cecd1e3d4de575ea9dffbd5e813c8dc
sahen <sahenshah95@gmail.com>
2025-08-13 19:17:20 +0100

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -190,4 +190,6 @@ def db_bootstrap_static():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
